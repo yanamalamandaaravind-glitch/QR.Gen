51786ee0d66f5473b5684791473e0e790136cd86
Rupee <flrupee@gmail.com>
2026-09-02 14:32:02 +0530

    Add ads verfaction

---
+++ backend/src/test/java/com/qrcode/qr_backend/QrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrcode.qr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
